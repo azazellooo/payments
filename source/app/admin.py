947bd56a5f38d1e2ad7402251777ea22540778da
karlygach <karygachkenenbaeva@gmail.com>
2022-02-08 17:01:35 +0600

    created models & registered in admin site

--- source/app/admin.py
+++ source/app/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
 
+from .models import Card, Purchase, STATUS_CHOICES
+
+
+def activate_cards(modeladmin, request, queryset):
+    for card in queryset:
+        card.status = STATUS_CHOICES[0][0]
+        card.save()
+
+
+def inactivate_cards(modeladmin, request, queryset):
+    for card in queryset:
+        card.status = STATUS_CHOICES[1][0]
+        card.save()
+
+
+@admin.register(Card)
+class CardAdmin(admin.ModelAdmin):
+    list_display = ['id', 'owner_name', 'number', 'issue_date',  'expiration_date', 'status']
+    list_filter = ['issue_date', 'status']
+    search_fields = ['owner_name', 'number', 'issue_date', 'status']
+    fields = ['owner_name', 'status']
+    readonly_fields = ['expiration_date', 'id', 'issue_date', 'number']
+    list_editable = ['status']
+    actions = [activate_cards, inactivate_cards]
+
+    activate_cards.short_description = 'Activate selected cards'
+    inactivate_cards.short_description = 'Inactivate selected cards'
+
+
+@admin.register(Purchase)
+class PurchaseAdmin(admin.ModelAdmin):
+    list_display = ['good', 'price', 'cc_number', 'date']
+    list_filter = ['cc_number']
+    search_fields = ['cc_number__number']
+
 # Register your models here.
--- source/app/models.py
+++ source/app/models.py
@@ -1,3 +1,42 @@
+import uuid
+from random import getrandbits
+from dateutil.relativedelta import relativedelta
+
 from django.db import models
+
+STATUS_CHOICES = [
+    ("activated", "active"),
+    ("inactivated", "inactive"),
+    ("expired", "expired"),
+]
+
+
+class Card(models.Model):
+    owner_name = models.CharField(max_length=120)
+    status = models.CharField(max_length=120, choices=STATUS_CHOICES)
+    issue_date = models.DateTimeField(auto_now_add=True)
+    number = models.CharField(unique=True, max_length=120)
+
+    def save(self, *args, **kwargs):
+        self.number = str(getrandbits(46))
+        super(Card, self).save(*args, **kwargs)
+
+    @property
+    def expiration_date(self):
+        return self.issue_date + relativedelta(months=18)
+
+    def __str__(self):
+        return self.number
+
+
+class Purchase(models.Model):
+    good = models.CharField(max_length=120)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    date = models.DateTimeField(auto_now_add=True)
+    cc_number = models.ForeignKey('app.Card', on_delete=models.CASCADE, related_name='purchases')
+
+    def __str__(self):
+        return f'{self.cc_number}: {self.good}'
+
 
 # Create your models here.
