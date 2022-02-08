7a9c03bd3fe72a1a287bcd4c7ed1bab3d63e5135
karlygach <karygachkenenbaeva@gmail.com>
2022-02-08 17:37:48 +0600

    minor fix

--- source/app/jobs/jobs.py
+++ source/app/jobs/jobs.py
@@ -5,7 +5,7 @@ from datetime import date
 def make_expired():
     today = date.today()
     for card in Card.objects.all():
-        if today == card.expiration_date.date():
+        if today >= card.expiration_date.date():
             card.status = STATUS_CHOICES[2][0]
             card.save()
             print(f'card {card.number} is expired.')
